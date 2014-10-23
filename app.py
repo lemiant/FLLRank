cc5e22c645b6b73e31f91a5832f9d61a6df97fc7
Alex Rodrigues <lemiant@gmail.com>
2014-10-23 10:21:33 -0700

    spacing

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ def solve():
     results_file = secure_filename(request.files['data'].filename)
     results_file = "results/"+datetime.now().strftime("%y-%m-%d_%H-%M-%S_")+results_file
     with open(results_file, "w") as out:
-    	writer = csv.writer(out)
-    	for row in results:
-    		writer.writerow(row)
+        writer = csv.writer(out)
+        for row in results:
+            writer.writerow(row)
 
     return render_template("results.html", errors=errors, results_file=results_file)
 
 @app.route("/results/<csv>")
 def results(csv):
-	return send_from_directory("results", csv)
+    return send_from_directory("results", csv)
 
 if __name__ == "__main__":
-	if os.getcwd() == "/var/www":
-    	app.run(host="0.0.0.0", port=80, debug=True)
-	else:
-    	app.run(debug=True)
+    if os.getcwd() == "/var/www":
+        app.run(host="0.0.0.0", port=80, debug=True)
+    else:
+        app.run(debug=True)
